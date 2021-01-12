4e31318b690fa8bd7d47ef999375a2eccc32bccb
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-12 16:59:40 -0500

    woot woot

--- program.js
+++ program.js
@@ -1,11 +1,11 @@
-console.log("Hello World");
-console.log("im all alone...");	
-console.log("there's no one here beside me");
-console.log("and my problems have all gone");
-console.log("ther is no one to deride me");
-console.log("but you got to have friends");
-console.log("hello Matt");
-console.log("hello world");
-console.log("hello Ashlyn");
-console.log("donkeyyy");
+console.log("colt 45 and 2 zig zags");
+console.log("baby that's all we need");	
+console.log("we can go to the park after dark");
+console.log("..");
+console.log("..");
+console.log("..");
+console.log("..");
+console.log("..");
+console.log("..");
+console.log("..");
 
