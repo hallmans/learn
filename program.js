d68f3fb4f168b2fc20d038a77f81aada31f6a0da
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-12 16:52:19 -0500

    shrek is KING

--- program.js
+++ program.js
@@ -1,11 +1,11 @@
 console.log("Hello World");
 console.log("im all alone...");	
-console.log("hello Dean");
-console.log("all by mysef");
-console.log("nobod here to guide mee");
-console.log("hello world");
+console.log("there's no one here beside me");
+console.log("and my problems have all gone");
+console.log("ther is no one to deride me");
+console.log("but you got to have friends");
 console.log("hello Matt");
 console.log("hello world");
 console.log("hello Ashlyn");
-console.log("hello world");
+console.log("donkeyyy");
 
