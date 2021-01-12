c6bd057aad59cd9535d2395c98292df34adfd2dc
Matthew Hallman <mhall587@gmail.com>
2021-01-12 12:19:03 -0500

    first version

---
+++ program.js
@@ -0,0 +1,15 @@
+console.log("hello oleorld");
+console.log("hello world");
+l
+console.log("hello world");
+console.log("Goodbye world"");
+console.log("hello world");
+console.errorr("hello world");
+console.log("hello earth")
+
+onsole.log("hello world");
+
+consOle.log("HELLO WORLD");CONSOLE.LOG("HELLO WORLD"
+
+
+
