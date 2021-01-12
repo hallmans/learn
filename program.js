df94fa1388c3a323c5aa3fa703e8e1c0eb1cca22
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-12 17:19:09 -0500

    comment

--- program.js
+++ program.js
@@ -1,22 +1,10 @@
-console.log("colt 45 and 2 zig zags");
-console.log("baby that's all we need");	
-console.log("we can go to the park after dark");
-console.log("..");
-console.log("..");
-console.log("..");
-console.log("..");
-console.log("..");
-console.log("..");
-console.log("..");
-
-console.log("Hello World");
 console.log("im all alone...");	
 console.log("there's no one here beside me");
 console.log("and my problems have all gone");
 console.log("ther is no one to deride me");
 console.log("but you got to have friends");
-console.log("hello Matt");
-console.log("hello world");
+console.log("I'm a flying, talking donkey");
+console.log("THat'll do donkey, that'll do");
 console.log("we can stay up late swapping manly stories and in the morning....");
 console.log(" I'M MAKING WAFFLES");
 console.log("donkeyyy");
