692f9d6745b3f8d06654daee5930f0af7bd6d4a0
Matthew Hallman <mhall587@gmail.com>
2021-01-12 17:12:26 -0500

    BWEAKFEST

--- program.js
+++ program.js
@@ -9,3 +9,16 @@ console.log("..");
 console.log("..");
 console.log("..");
 
+console.log("Hello World");
+console.log("im all alone...");	
+console.log("there's no one here beside me");
+console.log("and my problems have all gone");
+console.log("ther is no one to deride me");
+console.log("but you got to have friends");
+console.log("hello Matt");
+console.log("hello world");
+console.log("we can stay up late swapping manly stories and in the morning....");
+console.log(" I'M MAKING WAFFLES");
+console.log("donkeyyy");
+
+
