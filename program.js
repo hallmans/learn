d99fadd3c6e07f6884786d6631afd24fb31dec15
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-13 12:26:57 -0500

    Modeans #4

--- program.js
+++ program.js
@@ -6,3 +6,6 @@ console.log("You're made of spare parts, aren't ya bud?");
 console.log("And i suggest you let that one marinate.");
 console.log("Oh, get off the cross, we need the wood.");
 console.log("it's SUSHIS AND SUSHEMI'S");
+console.log("We've only got one shot at this. One chance. One win. You know? vomit on your mom's spaghetti, or whatever that talking singer says.");
+console.log("I dont give a shit about your kids.");
+console.log("figure it out.");
