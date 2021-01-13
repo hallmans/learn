5b8faccd86422fad07bdcc39194259ec6682115c
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-13 12:17:30 -0500

    more letterkenny

--- program.js
+++ program.js
@@ -2,3 +2,7 @@ console.log("that was well brought up, too bad you weren't");
 console.log("Does a duck with a boner drag weeds?");
 console.log(" it was a sick ostrish");
 console.log("allegedly");
+console.log("You're made of spare parts, aren't ya bud?");
+console.log("And i suggest you let that one marinate.");
+console.log("Oh, get off the cross, we need the wood.");
+
