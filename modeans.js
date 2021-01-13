4b1efda07f457b132da27eed40f67e84fa4fb649
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-13 12:45:35 -0500

    Modeans is on FIRE!

--- modeans.js
+++ modeans.js
@@ -1,8 +1,8 @@
-var fire = 0;
+var fire = 1;
 
-while (fire < 5) {
+while (fire < 11) {
 
-	console.log("This is modeans #", fire);
+	console.log("This is modeans #", fire + " How are ya now?");
 
 	fire = fire + 1;
 
