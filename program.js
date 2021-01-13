dfbbff80e6c560cedddaf2a6068ca7e20f072a44
Matthew Hallman <mhall587@gmail.com>
2021-01-13 12:11:28 -0500

    i heard...

--- program.js
+++ program.js
@@ -1,3 +1,4 @@
 console.log("that was well brought up, too bad you weren't");
 console.log("Does a duck with a boner drag weeds?");
-
+console.log(" it was a sick ostrish");
+console.log("allegedly");
