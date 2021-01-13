28e34118a9483f8f9698c2def7eb65ce90719b42
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-13 13:10:03 -0500

    Gail's Bar

--- modeans.js
+++ modeans.js
@@ -2,10 +2,19 @@ var fire = 1;
 
 while (fire < 11) {
 
-	console.log("This is modeans #", fire + " How are ya now?");
+	console.log("This is modeans #", fire ,"ferda ");
 
 	fire = fire + 1;
 
 }
 console.log(" wow that's how many fires? like", fire);
 
+console.log("Modean's keeps burning down, we are going to rename it Gail's");
+
+var flood = 1;
+
+while (flood < 6) {
+
+	console.log("Gail's flooded ", flood + "  times and she's pissed");
+	flood = flood + 1;
+}
