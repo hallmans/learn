9158391613d82c870440bde2b6ba0979c087a7c1
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-13 12:04:45 -0500

    we are doing Letterkenny quotes today.

--- program.js
+++ program.js
@@ -1,12 +1,3 @@
-console.log("im all alone...");	
-console.log("there's no one here beside me");
-console.log("and my problems have all gone");
-console.log("ther is no one to deride me");
-console.log("but you got to have friends");
-console.log("I'm a flying, talking donkey");
-console.log("THat'll do donkey, that'll do");
-console.log("we can stay up late swapping manly stories and in the morning....");
-console.log(" I'M MAKING WAFFLES");
-console.log("donkeyyy");
-console.log("that ws well brought up, too badyou weren't");
+console.log("that was well brought up, too bad you weren't");
+console.log("Does a duck with a boner drag weeds?");
 
