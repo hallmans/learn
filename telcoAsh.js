f915d35c4ba67a3176784651f948523d314b4311
Ashlyn Hallman <ashlynbhallman@gmail.com>
2021-01-15 12:52:28 -0500

    telco

---
+++ telcoAsh.js
@@ -0,0 +1,31 @@
+// banking progtram
+//day 5
+
+var checking = 10;
+
+function isDeposit(amt){
+        checking = checking + amt;
+	console.log("Your deposit was $" + amt ,"Your new balance is $" + checking);
+        return checking;
+}
+function isWithdraw(amt){
+        checking = checking + amt;
+	console.log("Your withdraw was $" + amt ,"Your new balance is $" + checking);
+	return checking;
+}
+isDeposit(600);
+isWithdraw(200);
+isDeposit(125);
+isWithdraw(40);
+isDeposit(23);
+isWithdraw(432);
+isDeposit(876);
+isWithdraw(11);
+isDeposit(60);
+isWithdraw(65);
+isDeposit(1000);
+isWithdraw(35);
+
+
+
+
