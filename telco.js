aaf8d502e384d6ea4714ae418ef3c53401c8f479
Matthew Hallman <mhall587@gmail.com>
2021-01-15 12:42:25 -0500

    yes

---
+++ telco.js
@@ -0,0 +1,18 @@
+var checking = 10;
+function deposit(amt) {
+	checking = checking + amt;
+	console.log("deposit", amt, "new balance", checking);
+	return checking;
+}
+function withdraw(amt) {
+	checking = checking - amt;
+	console.log("withdraw", amt, "new balance", checking);
+	return checking;
+}
+
+deposit(300);
+withdraw(100);
+deposit(2000);
+withdraw(600);
+deposit(45);
+withdraw(3000);
